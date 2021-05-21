202425303c7f26b43e4f9f12256ece94f6c90e2e
6010110455 <6010110455@psu.ac.th>
2021-05-21 15:54:45 +0700

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Image from 'next/image'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+
+      <main>
+        <h1 className="title">
+          <Link href="/posts/first-post">
+            <a>this page!</a>
+          </Link>
+        </h1>
+
+        <section className={utilStyles.headingMd}>
+        <p>[Your Self Introduction]</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+      </main>
+    </Layout>
+  )
+}
